0d8cd20570666fdf9b068573e9f491b3a68f3bc2
gabrielgutemberg <60550099+gabrielgutemberg@users.noreply.github.com>
2026-05-07 20:59:00 -0300

    atividade

--- index.php
+++ index.php
@@ -1,8 +1,45 @@
 <h1>Atividade: Classe Pessoa (4 pilares da programacao)</h1>
 <?php   
 
+class Pessoa{
+    protected $nome;
+    protected $cpf;
+    protected $idade;
+
+    public function __construct($nome, $cpf, $idade){
+        $this->nome = $nome;
+        $this->cpf = $cpf;
+        $this->idade = $idade;
+    }
+
+    public function apresentar(){
+        echo "Olá eu sou " . $this->nome;
+    }
+}
+// herança
+class Estudante extends Pessoa{
+    public $matricula;
+
+    public function __construct($nome, $cpf, $idade, $matricula){
+        $this->matricula = $matricula;
+        parent::__construct($nome, $cpf, $idade);
+    }
+    public function apresentar(){
+        echo "Olá eu sou o estudante ". $this->nome;
+    }
+}
+class Servidor extends Pessoa{
+    private $salario;
+    public function __construct($nome, $cpf, $idade, $salario = 1500){
+        $this->salario = $salario;
+        parent::__construct($nome, $cpf, $idade);
+    }
+}
 
 
-
-
+$pessoa_x = new Estudante("Felipe","000.000.000-00","20",20000);
+$servidor_x = new Servidor("Victoria","111.111.111-11","23");
+$pessoa_x->apresentar();
+echo "<br>";
+$servidor_x->apresentar();
 ?>
---
+++ pdo.php
@@ -0,0 +1,21 @@
+<?php 
+
+
+$pdo = new PDO("mysql:host=localhost;dbname=meu_banco","root","");
+if(!$pdo) die("A Conexao com o banco falhou");
+
+
+echo"<h3>Lista de usuários</h3><br>";
+
+$identificador = 2;
+$sql = "SELECT * FROM usuarios";
+$stmt = $pdo->prepare($sql);
+$stmt->execute([
+    "id"=> $identificador,
+]);
+
+foreach($stmt->fetchAll(PDO::FETCH_ASSOC) as $row){
+    echo "Nome: ". $row["nome"] . "<br>";
+    echo "Email: ". $row["email"] . "<br>";
+}
+?>
