bd84d157bfe77f1f89ca18c4c213838079b95038
Gabriel <60550099+gabrielgutemberg@users.noreply.github.com>
2026-05-08 20:43:36 -0300

    atividade poo +  pdo crud

--- index.php
+++ index.php
@@ -18,28 +18,54 @@ class Pessoa{
 }
 // herança
 class Estudante extends Pessoa{
-    public $matricula;
+    public $curso;
 
-    public function __construct($nome, $cpf, $idade, $matricula){
-        $this->matricula = $matricula;
+    public function __construct($nome, $cpf, $idade, $curso){
+        $this->curso = $curso;
         parent::__construct($nome, $cpf, $idade);
     }
     public function apresentar(){
-        echo "Olá eu sou o estudante ". $this->nome;
+        echo "Olá eu sou o estudante ". $this->nome. " eu sou do curso de " . $this->curso;
     }
 }
 class Servidor extends Pessoa{
     private $salario;
-    public function __construct($nome, $cpf, $idade, $salario = 1500){
+    public function __construct($nome, $cpf, $idade,$funcao, $salario = 1500){
         $this->salario = $salario;
+        $this->funcao = $funcao;
+        parent::__construct($nome, $cpf, $idade);
+    }
+    public function apresentar(){
+        echo "Olá eu sou ". $this->nome. " e sou " . $this->funcao;
+    }
+}
+class Professor extends Pessoa{
+    private $salario;
+    public $curso;
+    public function __construct($nome, $cpf, $idade, $curso, $salario = 1500){
+        $this->salario = $salario;
+        $this->curso = $curso;
+        parent::__construct($nome, $cpf, $idade);
+
+    }
+    public function apresentar(){
+        echo "Olá eu sou o professor ".$this->nome. " do curso de ". $this->curso;
+    }
+}
+class Visitante extends Pessoa{
+    public function __construct($nome, $cpf, $idade){
         parent::__construct($nome, $cpf, $idade);
     }
 }
-
-
-$pessoa_x = new Estudante("Felipe","000.000.000-00","20",20000);
-$servidor_x = new Servidor("Victoria","111.111.111-11","23");
+$pessoa_x = new Estudante("Felipe","000.000.000-00","20","Administração");
+$servidor_x = new Servidor("Victoria","111.111.111-11","23","bolsista");
+$professor_x = new Professor("Julia","222.222.222-22","33","SI");
+$visitante_x = new Visitante("Gabriel","333.333.333-22","19");
 $pessoa_x->apresentar();
 echo "<br>";
 $servidor_x->apresentar();
+echo "<br>";
+$professor_x->apresentar(); 
+echo "<br>";
+$visitante_x->apresentar(); 
 ?>
--- pdo.php
+++ pdo.php
@@ -7,15 +7,38 @@ if(!$pdo) die("A Conexao com o banco falhou");
 
 echo"<h3>Lista de usuários</h3><br>";
 
-$identificador = 2;
-$sql = "SELECT * FROM usuarios where id = id";
+
+$sql = "INSERT INTO usuarios(nome, email) VALUES(:nome, :email)";
 $stmt = $pdo->prepare($sql);
 $stmt->execute([
-    "id"=> $identificador,
+    ":nome" => "Jubileu",
+    ":email" => "jubileu@email.com"
 ]);
+
+$identificador = 1;
+$sql = "UPDATE usuarios SET nome = :nome, email = :email WHERE id = :id";
+$stmt = $pdo->prepare($sql);
+$stmt->execute([
+    ":nome" => "Gabriel",
+    ":email" => "gabriel@email.com",
+    ":id" => $identificador
+]);
+
+
+$identificador = 5;
+$sql = "DELETE FROM usuarios WHERE id = :id";
+$stmt = $pdo->prepare($sql);
+$stmt->execute([
+    ":id" => $identificador
+]);
+
+$sql = "SELECT * FROM usuarios";
+$stmt = $pdo->prepare($sql);
+$stmt->execute();
 
 foreach($stmt->fetchAll(PDO::FETCH_ASSOC) as $row){
     echo "Nome: ". $row["nome"] . "<br>";
     echo "Email: ". $row["email"] . "<br>";
 }
+
 ?>
